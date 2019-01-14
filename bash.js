d85ca5d19cab6e850b001c4fdc0af3b7f1c6ab66
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2019-01-14 13:00:55 -0500

    modules

--- bash.js
+++ bash.js
@@ -1,13 +1,16 @@
+const pwd = require('./pwd.js');
+const ls = require('./ls.js');
 //Output a prompt
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
-process.stdin.on('data', (data) => {
+process.stdin.on('data', data => {
   const cmd = data.toString().trim();
-  
-  process.stdout.write('You typed: ' + cmd);
+
+  process.stdout.write('You typed: ' + cmd + '\n');
   if (cmd === 'pwd') {
-    console.log(`\n${ process.cwd()}`);
+    pwd();
+  } else if (cmd === 'ls') {
+    ls();
   }
-  process.stdout.write('\nprompt > ');
-})
+});
