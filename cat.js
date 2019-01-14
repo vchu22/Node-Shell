f9353a161c99bd04c0739fe47e8c8fea1718a535
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2019-01-14 14:36:41 -0500

    fixed cat.js

--- cat.js
+++ cat.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports = filename => {
-  let str = fs.readFileSync(filename);
+  let str = fs.readFile(filename);
   process.stdout.write(str);
   process.stdout.write('prompt > ');
 };
